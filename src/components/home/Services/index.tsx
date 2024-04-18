0f3c1c1c32d30bf2557150c9a6461376b2172faf
wigor <wigor21@hotmail.com>
2024-04-17 22:29:02 -0300

    feat: created services

--- src/components/home/Services/index.tsx
+++ src/components/home/Services/index.tsx
@@ -4,21 +4,42 @@ import Image from "next/image";
 
 export const Service = ({ object }: HomeProps) => {
   return (
-    <section className={styles.containerAbout}>
-      <article className={styles.innerAbout}>
-        <h1 className={styles.title}>Sobre</h1>
-        <p>{object.metadata.about.description}</p>
-      </article>
+    <>
+      <section id="servicos" className={styles.containerAbout}>
+        <article className={styles.innerAbout}>
+          <h1 className={styles.title}>Sobre</h1>
+          <p>{object.metadata.about.description}</p>
+        </article>
 
-      <div className={styles.bannerAbout}>
-        <Image
-          className={styles.imageAbout}
-          alt="Imagem ilustrativa sobre a empresa"
-          quality={100}
-          fill={true}
-          src={object.metadata.about.banner.url}
-        />
-      </div>
-    </section>
+        <div className={styles.bannerAbout}>
+          <Image
+            className={styles.imageAbout}
+            alt="Imagem ilustrativa sobre a empresa"
+            quality={100}
+            fill={true}
+            src={object.metadata.about.banner.url}
+          />
+        </div>
+      </section>
+
+      <h2 className={styles.servicesTitle}>Conheça nossos serviços</h2>
+
+      <section className={styles.services}>
+        {object.metadata.services.map((service) => (
+          <article key={service.description} className={styles.service}>
+            <div className={styles.innerService}>
+              <Image
+                className={styles.imageService}
+                alt="Imagem do serviço"
+                quality={100}
+                fill={true}
+                src={service.image.url}
+              />
+            </div>
+            <p>{service.description}</p>
+          </article>
+        ))}
+      </section>
+    </>
   );
 };
